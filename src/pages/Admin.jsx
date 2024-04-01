d56028aed90866538cf6217019ca3922bed5819a
Chen <chen100030@gmail.com>
2024-04-01 13:25:48 +0300

    teams names

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { prominent } from 'color.js'
+import { ImageColorPicker } from 'react-image-color-picker';
 
 import { gameService } from '../services/game.service.js'
 import { utilService } from '../services/util.service.js'
@@ -9,27 +10,30 @@ export function Admin() {
     const [game, setGame] = useState(gameService.getEmptyGame())
 
     useEffect(() => {
-        console.log('game:', game)
-
         // const date = new Date("2024-04-02 11:00")
         // console.log('date:', date.getTime())
-        if (game.logo) {
-            console.log(game.logo)
-        }
+        console.log('game:', game)
+        console.log('game.teams.length:', game.teams?.length)
 
     }, [game])
 
     function onHandleChange(ev) {
         const { name, value } = ev.target
-        setGame(prevGame => ({ ...prevGame, [name]: value }))
+        if (name === 'numOfTeams') {
+            setGame(prevGame => {
+                const diff = value - (prevGame.teams?.length || 0)
+                if (diff > 0) return { ...prevGame, teams: (prevGame.teams?.length) ? [...prevGame.teams, { name: '' }] : [{ name: '' }] }
+                else return { ...prevGame, teams: prevGame.teams.filter((t, i) => i < value) }
+            })
+        } else {
+            setGame(prevGame => ({ ...prevGame, [name]: value }))
+        }
     }
 
     async function onChangeImg(ev) {
-        console.log('onChangeImg')
         try {
             // setIsLoading(true)
             const media = await utilService.uploadImgToCloudinary(ev)
-            console.log('media:', media)
             setGame(prevGame => ({ ...prevGame, logo: media }))
             getColorsFromImg(media.url)
         } catch (err) {
@@ -45,9 +49,22 @@ export function Admin() {
         setGame(prevGame => ({ ...prevGame, colors }))
     }
 
+    function onHandleTeamNameChange(ev, i) {
+        const { value } = ev.target
+        setGame(prevGame => {
+            prevGame.teams[i].name = value
+            return { ...prevGame }
+        })
+
+    }
+
     function onSubmitForm(ev) {
         ev.preventDefault()
     }
+
+    function onHandleColorPick(color) {
+        console.log('Selected color:', color); // Selected color: rgb(101, 42, 65)
+    };
 
     return (
         <section className="admin rtl">
@@ -72,12 +89,30 @@ export function Admin() {
                 <input type="file" name="logo" id="logo" onChange={onChangeImg} />
                 {game.logo && <img className="logo-img" src={game.logo.url} />}
 
-                {game.colors.length && <ul className="colors">
-                    {game.colors.map((color, i) => <li>
+                {game.colors && <ul className="colors">
+                    {game.colors.map((color, i) => <li key={color}>
                         <input type="color" name="color1" id={`color${i + 1}`} value={color} />
                     </li>)}
                 </ul>}
 
+                {/* color picker on the logo image */}
+                {/* {game.logo && <div className="img-color-picker-container">
+                    <ImageColorPicker
+                    onColorPick={onHandleColorPick}
+                    imgSrc={game.logo.url}
+                    zoom={1}
+                    />
+                </div>} */}
+
+                <label htmlFor="numOfTeams">מספר הקבוצות</label>
+                <input type="number" name="numOfTeams" id="numOfTeams" value={game.teams?.length || 0} onChange={onHandleChange} />
+
+                {game.teams && <ul className="teams">
+                    {game.teams.map((team, i) => <li key={i}>
+                        <label htmlFor="teamName">שם קבוצה  {i + 1}</label>
+                        <input type="text" name="teamName" id="teamName" value={game.teams[i].name} onChange={() => onHandleTeamNameChange(event, i)} />
+                    </li>)}
+                </ul>}
 
             </form>
         </section>
