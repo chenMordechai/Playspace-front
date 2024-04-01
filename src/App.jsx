112b7d3ca7bf72913ecb6b19ec8ba669384ac2fd
Chen <chen100030@gmail.com>
2024-04-01 09:26:23 +0300

    added scss architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
+import './assets/style/main.scss'
+
 import { Login } from './pages/Login'
 import { Home } from './pages/Home'
 import { Admin } from './pages/Admin'
 import { NavLinks } from './cmps/NavLinks'
-function App() {
 
+
+function App() {
   return (
     <Router>
       <section className="main-layout">
