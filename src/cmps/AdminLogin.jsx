f61b090bda3af1321920820e6b0a34b56d54d243
Chen <chen100030@gmail.com>
2024-05-02 23:14:34 +0300

    onclick input eye

--- src/cmps/AdminLogin.jsx
+++ src/cmps/AdminLogin.jsx
@@ -1,9 +1,18 @@
 import spacegameLogoBlue from '../assets/img/spacegame-logo-blue.png'
 import password from '../assets/img/password.png'
 import eye from '../assets/img/eye.png'
-
+import { useRef } from 'react'
 
 export function AdminLogin({ loggedinUser, handleSubmitAdminForm, credentials, handleChange }) {
+
+    const input = useRef()
+
+    function onChangeInputType() {
+        // input.type 
+        console.log('input.current.type :', input.current.type)
+        input.current.type = (input.current.type === 'password') ? 'text' : 'password'
+    }
+
     return (
         <section className="admin-login">
             <img className="spacegame-logo-blue" src={spacegameLogoBlue} />
@@ -11,8 +20,8 @@ export function AdminLogin({ loggedinUser, handleSubmitAdminForm, credentials, h
             <form className="admin-login-form" onSubmit={handleSubmitAdminForm}>
                 <span>Hello {loggedinUser.name}</span>
                 <img className="input-img password" src={password} />
-                <img className="input-img eye" src={eye} />
-                <input placeholder="Password" type="password" id="password" name="password" value={credentials.password} onChange={handleChange} required />
+                <img className="input-img eye" onClick={onChangeInputType} src={eye} />
+                <input ref={input} placeholder="Password" type="password" id="password" name="password" value={credentials.password} onChange={handleChange} required />
 
                 <button type="submit" disabled={!credentials.password} >Admin Login</button>
             </form>
